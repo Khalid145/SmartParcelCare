2ad1000de8ca751200e9b3e9f7aab675a2e59e61
Khalid Mohamed <khalid145@hotmail.co.uk>
2018-05-22 18:29:56 +0100

    Updated pyfirmata.py

--- Pyfirmata Source Code/Pyfirmata.py
+++ Pyfirmata Source Code/Pyfirmata.py
@@ -1,27 +1,25 @@
 import serial,json,time
-#import boto3
+import boto3
 import requests
+import re
+import simplejson
 
-
-
-#client = boto3.client('sns')
+client = boto3.client('sns')
 arn = "arn:aws:sns:us-west-2:524203992316:NotifyMe"
 
-arduinoSerialData = serial.Serial('/dev/cu.usbmodem1421', 9600)
-def test():
-    while(1==1):
-        if(arduinoSerialData.inWaiting()>0):
-            myData = arduinoSerialData.readline()
-            try:
-                response_json = response_json.decode('utf-8').replace('\0', '')
-                jsonData = json.loads(myData)
-            except:
-                print('bad json: ', response_json)
-            return jsonData
+arduinoSerialData = serial.Serial('/dev/ttyACM0', 9600)
 
-    
-        #jsonData = json.loads(myData)
-        #print (jsonData)
+while(1==1):
+    if(arduinoSerialData.inWaiting()>0):
+        myData = arduinoSerialData.readline()
+
         
-        #response = client.publish(TopicArn=arn,Message=jsonData)
-        #print("Response: {}".format(response))
+        print(myData)
+        #jsonData = json.loads(myData)
+        #print("JSON")
+        
+        #jsonData = json.dumps(myData)
+        print (myData)
+        
+        response = client.publish(TopicArn=arn,Message=myData)
+        print("Response: {}".format(response))
